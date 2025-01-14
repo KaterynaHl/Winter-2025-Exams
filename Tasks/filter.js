9d8393f8f094d4bea0f3ce42f96ed38e977aa2a4
KaterynaHl <glebakata50@gmail.com>
2025-01-15 01:05:27 +0200

    Add validation to ensure the second argument is a string

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -4,6 +4,8 @@
 const Filter = (array, typeName) => {
   if (!Array.isArray(array))
     throw new TypeError("First argument must be an array");
+  if (typeof typeName !== "string")
+    throw new TypeError("Second argument must be a string");
 
   return array.filter((item) => typeof item === typeName);
 };
