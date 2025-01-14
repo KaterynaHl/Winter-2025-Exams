ffa1f4ed8c8c372e2379b0cf44388d80f76a5ba2
KaterynaHl <glebakata50@gmail.com>
2025-01-14 23:45:07 +0200

    Use const for key arrays

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,8 +1,8 @@
 // Compare two dictionaries
 
 const compare = (firstDict, secondDict) => {
-  let firstKeys = Object.keys(firstDict);
-  let secondKeys = Object.keys(secondDict);
+  const firstKeys = Object.keys(firstDict);
+  const secondKeys = Object.keys(secondDict);
   if (firstKeys.length !== secondKeys.length) return false;
 
   return firstKeys.every((key) => firstDict[key] === secondDict[key]);
