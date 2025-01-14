8b00180ea71ffb05dbb37b64212161517469b9c1
KaterynaHl <glebakata50@gmail.com>
2025-01-15 01:03:34 +0200

    Add validation to ensure the first argument is an array

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,7 +1,11 @@
 // Refactor following solution
 // Filter array by type name
 
-const Filter = (array, typeName) =>
-  array.filter((item) => typeof item === typeName);
+const Filter = (array, typeName) => {
+  if (!Array.isArray(array))
+    throw new TypeError("First argument must be an array");
+
+  return array.filter((item) => typeof item === typeName);
+};
 
 module.exports = Filter;
