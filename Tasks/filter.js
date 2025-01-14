81bffa41f0cb471e2ec7445de5bd0c5a3eaf2203
KaterynaHl <glebakata50@gmail.com>
2025-01-15 00:51:56 +0200

    Avoid mutating the input array by creating a new array

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,16 +1,9 @@
 // Refactor following solution
 // Filter array by type name
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
-      remove.unshift(x);
-    }
-  }
-  for (x of remove) T.splice(x, 1);
-  return T;
+const Filter = (T, t) => {
+  const result = T.filter((item) => typeof item === t);
+  return result;
 };
 
 module.exports = Filter;
