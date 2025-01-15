5c482d1d846a3daac83349280fc24f0bc75c0318
KaterynaHl <glebakata50@gmail.com>
2025-01-15 03:11:07 +0200

    Use a local variable for months instead of a global one

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,27 +1,27 @@
 // Refactor following solution
 // Get month number
 
-Months = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'may',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'oct',
-  'nov',
-  'dec',
+const months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
 ];
 
-Month = (s) => {
-  l = Months.length;
+const month = (s) => {
+  l = months.length;
   for (let i = 0; i < l; i++) {
-    if (s.toLowerCase().startsWith(Months[i])) return i + 1;
+    if (s.toLowerCase().startsWith(month[i])) return i + 1;
   }
   return -1;
 };
 
-module.exports = Month;
+module.exports = month;
